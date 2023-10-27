47b10f591c437dfaab2fc7fed5c9f2a356dfe8c2
Mateusz <mkaminski.contact@gmail.com>
2023-10-27 10:26:57 +0200

    changed loader localisation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root";
 import HomePage from "./pages/Home.";
 import EditEventPage from "./pages/EditEvent";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader} from "./pages/Events";
 import EventsDetailPage from "./pages/EventsDetail";
 import NewEventPage from "./pages/NewEvent";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -20,15 +20,7 @@ const router = createBrowserRouter([
 					{
 						index: true,
 						element: <EventsPage />,
-						loader: async () => {
-							const response = await fetch("http://localhost:8080/events");
-							if (!response.ok) {
-								// ....
-							} else {
-								const resData = await response.json();
-								return resData.events;
-							}
-						},
+						loader: eventsLoader,
 					},
 					{ path: ":eventId", element: <EventsDetailPage /> },
 					{ path: "new", element: <NewEventPage /> },
